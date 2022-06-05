07a9cb7706a0d48db840a5c8b8c096e259b4c036
Sophia Okito <okitooghenetega@gmail.com>
2022-06-05 17:27:54 +0100

    remove postgres crediential

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: "postgres://downstream_ia94_user:J96xl6ej519dwtE0OgxBVGVv30pT1xix@dpg-caeb0kb97ejce0be5js0-a.oregon-postgres.render.com/downstream_ia94",
+    url: "",
 
     synchronize: true,
     logging: true,
